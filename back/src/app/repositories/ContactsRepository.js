9feebad86fa571376ea49fd743b9c69185ab4f3c
Hugo Ramon <ramone.techie@gmail.com>
2023-01-31 11:07:59 -0300

    Added routes to get and delete a contact

--- back/src/app/repositories/ContactsRepository.js
+++ back/src/app/repositories/ContactsRepository.js
@@ -1,19 +1,37 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Hugo',
     email: 'ramone.techie@gmail.com',
     phone: '123456789',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Ramon',
+    email: 'hugo.techie@gmail.com',
+    phone: '567894321',
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
     return new Promise((resolve) => {
-      resolve(contacts);
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
