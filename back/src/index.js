04e73fa6ccafdbe7d4c8c2d32a072886fb7d2343
Hugo Ramon <ramone.techie@gmail.com>
2023-01-31 23:33:45 -0300

    Added middleware and created route to create new contact with its vallidations

--- back/src/index.js
+++ back/src/index.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => console.log('💻 Server is running at http://localhost:3000'));
