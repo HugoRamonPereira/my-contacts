135d42609ab4c6daffc681e66506cf4c0444efd2
Hugo Ramon <ramone.techie@gmail.com>
2023-02-20 18:37:05 -0300

    Added cors and errorHandler middlewares

--- back/src/index.js
+++ back/src/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+// This is the header that allows the CORS problem
+// it is allowing the requests to be made on every port - WILDCARD
+// Its important to define the CORS middleware before the routes, the order is important
+app.use(cors);
 app.use(routes);
 
 // This is an Error Handler Middleware, because it has 4 arguments and starting with an error
-app.use((error, request, response, next) => {
-  console.log('&&& Error Handler');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('💻 Server is running at http://localhost:3000'));
+app.listen(3001, () => console.log('💻 Server is running at http://localhost:3001'));
