a0fe73598b8b26d13d64eef8c187d58f16248743
Hugo Ramon <ramone.techie@gmail.com>
2023-03-09 10:20:34 -0300

    Created HttpClient to handle the http requests

--- front/src/services/ContactService.js
+++ front/src/services/ContactService.js
@@ -1,13 +1,16 @@
-import delay from '../utils/delay';
+import HttpClient from './utils/HttpClient';
 
 class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(
-      `http://localhost:3001/contacts?orderBy=${orderBy}`,
-    );
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
 
-    await delay(600);
-    return response.json();
+  async listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
+  }
+
+  async createContact(contact) {
+    return this.httpClient.post('/contacts', contact);
   }
 }
 
---
+++ front/src/services/utils/HttpClient.js
@@ -0,0 +1,16 @@
+import delay from '../../utils/delay';
+
+class HttpClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async get(path) {
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    await delay(600);
+    return response.json();
+  }
+}
+
+export default HttpClient;
