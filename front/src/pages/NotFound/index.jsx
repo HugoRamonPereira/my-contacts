9677604c847f36f7b0d237c5c8bfc139d2e76e7c
Hugo Ramon <ramone.techie@gmail.com>
2023-10-23 00:28:34 -0300

    removed duplicated file and folder

---
+++ front/src/pages/NotFound/index.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import * as Styled from './styles';
+
+export default function NotFound() {
+  const navigate = useNavigate();
+  return (
+    <Styled.Container>
+      <h1>Error 404</h1>
+      <h2>Page not found!</h2>
+      <Button
+        type="button"
+        onClick={() => navigate('/')}
+      >
+        Back to home
+      </Button>
+    </Styled.Container>
+  );
+}
